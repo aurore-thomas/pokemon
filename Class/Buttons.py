4e7222ffa1e921fed3ff2f571cf8a83396d33c2d
aurore-thomas <aurore.thomas@laplateforme.io>
2023-03-08 09:06:53 +0100

    [FIX] Buttons with hovering color

--- Class/Buttons.py
+++ Class/Buttons.py
@@ -1,17 +1,19 @@
 import pygame as pg
+from Class.Settings import *
 
 class Button:
-    def __init__(self, image, text, font, pos, size, basic_color, hovering_color, action):
+    def __init__(self, image, text_input, font, pos, size, basic_color, hovering_color, action):
         self.image = image
-        self.action = action
-        self.font = font 
-        self.text = font.render(text, True, basic_color)
-        self.x_pos = pos[0]
-        self.y_pos = pos[1]
-        self.x_size = size[0]
-        self.y_size = size[1]
+        self.size = size
+        self.text_input = text_input
+        self.font = font
         self.basic_color = basic_color
         self.hovering_color = hovering_color
+        self.text = font.render(self.text_input, True, self.basic_color)
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
+        
+        self.action = action
         if image is None:
             self.image = self.text
         else:
@@ -22,18 +24,25 @@ class Button:
     def draw(self, win):
         if self.image is not None:
             win.blit(self.image, self.rect)
-        win.blit(self.text, self.rect)
+        win.blit(self.text, self.text_rect)
 
-    def button_clicked(self, win):
-        self.mouse = pg.mouse.get_pos() # It gives us information about the mouse's position
-        self.click = pg.mouse.get_pressed() # It gives information about the number of click
-        if self.image == self.text :
-            if (self.x_pos + self.x_size) > self.mouse[0] > self.x_pos and (self.y_pos + self.y_size) > self.mouse[1] > self.y_pos:
-                self.basic_color = self.hovering_color
-                win.blit(self.text, self.rect)
-                pg.display.update()
-        if self.click[0] == 1 and self.action != None:
+    def button_clicked(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
             self.action()
-                
-        
-        
+    
+    def color_hover(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            self.text = self.font.render(self.text_input, True, self.hovering_color)
+        else:
+            self.text = self.font.render(self.text_input, True, self.basic_color)
+
+    # def button_clicked(self, win):
+    #     self.mouse = pg.mouse.get_pos() # It gives us information about the mouse's position
+    #     self.click = pg.mouse.get_pressed() # It gives information about the number of click
+    #     if self.image == self.text :
+    #         if (self.x_pos + self.x_size) > self.mouse[0] > self.x_pos and (self.y_pos + self.y_size) > self.mouse[1] > self.y_pos:
+    #             self.basic_color = self.hovering_color
+    #             win.blit(self.text, self.rect)
+    #             pg.display.update()
+    #     if self.click[0] == 1 and self.action != None:
+    #         self.action()
